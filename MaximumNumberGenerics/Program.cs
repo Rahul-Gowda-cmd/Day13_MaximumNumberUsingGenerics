0060e40dde6c5d10d949b75221e251b472bc350d
Rahul prabhu <rahul.prabu.07@gmail.com>
2021-08-11 23:15:04 +0530

    feat:Added integer Maximum number

--- MaximumNumberGenerics/Program.cs
+++ MaximumNumberGenerics/Program.cs
@@ -7,7 +7,10 @@ namespace MaximumNumberGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics program");
-            
+            Console.WriteLine("Find maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
+            Console.WriteLine(output);
+
         }
     }
 }
