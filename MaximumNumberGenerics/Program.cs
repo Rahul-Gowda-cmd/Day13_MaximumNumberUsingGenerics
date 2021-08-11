28f082b3d4901b519c4d5f005bc51e0a01174e6c
Rahul Gowda <65938264+Rahul-Gowda-cmd@users.noreply.github.com>
2021-08-11 23:26:25 +0530

    Update Program.cs

--- MaximumNumberGenerics/Program.cs
+++ MaximumNumberGenerics/Program.cs
@@ -10,7 +10,6 @@ namespace MaximumNumberGenerics
             Console.WriteLine("Find maximum Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(22, 33, 44);
             Console.WriteLine(output);
-
         }
     }
 }
